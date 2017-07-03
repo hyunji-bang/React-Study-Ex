e2d67f26fe714a9f65428e6b6108453ce847b865
hyunji-bang <groovehyunji@gmail.com>
2017-07-03 16:00:10 +0900

    Modifying index.js

--- search-compo/src/index.js
+++ search-compo/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react';
+import {render} from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Search extends Component {
+    render() {
+        return (
+            <div>
+                Search Term: <input type="search" value="React"/>
+            </div>
+        );
+    }
+}
+
+render(<Search />, document.body);
