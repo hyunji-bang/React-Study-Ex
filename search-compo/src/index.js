0a611af5d1bfcf6221a80d64eebd35a1e55ef4dd
hyunji-bang <groovehyunji@gmail.com>
2017-07-09 23:54:34 +0900

    Add constructor

--- search-compo/src/index.js
+++ search-compo/src/index.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import {render} from 'react-dom';
 
 class Search extends Component {
+    constructor(){
+        super();
+        this.state = {
+            searchTerm: "React"
+        }
+    }
     render() {
         return (
             <div>
-                Search Term: <input type="search" value="React"/>
+                Search Term: <input type="search" value={this.state.searchTerm}/>
             </div>
         );
     }
